de421979c7d89f16078b93836b6366196b3d775c
Brandon Streiff <git@streiff.net>
2018-02-11 13:42:09 -0600

    use the pixel generator

--- tools/gfx_display.py
+++ tools/gfx_display.py
@@ -38,32 +38,12 @@ screen = pygame.display.set_mode((sprite.width * 8, sprite.height * 8))
 pygame.display.set_caption("gfx_display")
 screen.fill((123, 45, 67))
 
-tiles = []
-
 pal = palettes[sprite.flags & 0x3]
 
-for b in range(0, 32*sprite.width*sprite.height):
-    value = sprite.data[b]
-    v1 = (value & 0xF0) >> 4
-    v2 = (value & 0x0F)
-    # Get the position within this VDP tile
-    sub_y = int((b % 32) / 4)
-    sub_x = int(b % 4)
-
-    # Draw the tiles in VDP order; this is "down, then across"
-    # VDP draw order is (for 4x4)
-    #  0  4  8  C
-    #  1  5  9  D
-    #  2  6  A  E
-    #  3  7  B  F
-    t = int(b / 32)
-    x = int(t / sprite.height)*4 + sub_x
-    y = int(t % sprite.height)*8 + sub_y
-
-    if (v1 != 0):
-        pygame.draw.rect(screen, pal[v1].rgb(), (x*2, y, 1, 1))
-    if (v2 != 0):
-        pygame.draw.rect(screen, pal[v2].rgb(), (x*2+1, y, 1, 1))
+for pixel in sprite.pixel_generator():
+    (x, y, index) = pixel
+    if index != 0:
+        pygame.draw.rect(screen, pal[index].rgb(), (x, y, 1, 1))
 
 pygame.display.flip()
 
